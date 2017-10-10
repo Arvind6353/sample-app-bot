a284c4e9cff5e0f0d3505b8dee03f02914be2f32
T A <arvta@paypalcorp.com>
2017-10-10 14:54:25 +0530

    s

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 const BootBot = require('bootbot');
+console.log('helloooow--------------->')
 
 const bot = new BootBot({
   accessToken: 'EAAN3s3DTH3sBAGeceVOiKV1q9SPzXLZB1nZBvnUppliWecQgeVCwcVTMQovfT4J4awZBh6foS5Ir2qvxQ3BCBOu8k7QSKIEGn7ZA2AIQ9c6APCvZAlZBpGYEkhkJeRwHeYs9ZAENwKB0aMqW1qspfmJWd8LZArZCgw60CiuIVJv5VxQZDZD',
@@ -11,5 +12,7 @@ bot.on('message', (payload, chat) => {
   const text = payload.message.text;
   chat.say(`Echo: ${text}`);
 });
-
-bot.start();
+console.log(process.env)
+console.log(process.env.PORT)
+console.log(process.env['PORT'])
+bot.start(process.env.PORT);
